58fef422a67c4f4f6f56ceff4dc79f818da250f0
hermantry <hermantrym@gmail.com>
2020-11-25 10:17:30 +0700

    Initial Commit

---
+++ basic/createTableSecond.sql
@@ -0,0 +1,10 @@
+/**
+ * create new table
+ */
+
+CREATE TABLE second( -- define name table;
+
+	id int identity(1, 1) primary key not null, -- index auto increment as primary key;
+	namaMataKuliah nchar(30) not null
+
+)
