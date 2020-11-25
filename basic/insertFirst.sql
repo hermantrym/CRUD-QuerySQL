24e2bb3ab0f074e074fd8491e1cf9b00d0b00df5
hermantry <hermantrym@gmail.com>
2020-11-25 10:18:16 +0700

    Initial Commit

---
+++ basic/insertFirst.sql
@@ -0,0 +1,11 @@
+/**
+ * insert data table first
+ */
+
+INSERT INTO [basic].[dbo].[first] -- name database, name schemas, name table;
+
+VALUES	('Hermantrm', '113923123', 'Russia', 18), -- name, nim, address, age;
+		('Surya Insomnia', '34544324', 'Baturaden', 19),
+		('Morhetz', '824673223', 'Moskow', 25),
+		('TechLead', '453458435', 'Japan', 23),
+		('Snowden', '3274326473', 'Latvia', 29)
