625372bac997ea5a7221f57928eb0508e758eaa4
hermantry <hermantrym@gmail.com>
2020-11-25 10:18:42 +0700

    Initial Commit

---
+++ basic/insertSecond.sql
@@ -0,0 +1,12 @@
+/**
+ * insert data table second
+ */
+
+INSERT INTO [basic].[dbo].[second] -- define name database, schemas, name tabel;
+
+VALUES
+		('Pemrograman Berorientasi Objek'),
+		('Matematika Diskrit'),
+		('Kalkulus'),
+		('Pemrograman Dasar'),
+		('Fisika')
